afd429aa144fe1701c8fbcbd5f47a8b04ab485ec
VenomVertigo <xxvitalassassinxx@aol.com>
2017-11-11 13:19:21 -0800

    Fixed HHKB layout

--- keyboards/dz60/keymaps/hhkb/keymap.c
+++ keyboards/dz60/keymaps/hhkb/keymap.c
@@ -1,6 +1,3 @@
-/*  -*-  eval: (turn-on-orgtbl); -*-
- * default HHKB Layout
- */
 #include "dz60.h"
 
 #define BASE 0
@@ -17,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Cont  | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Shift | Z | X | C | V | B | N | M |  | . | / | Shift | Fn0 |       |   |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----qmk_firmware/keyboards/dz60/keymaps+-------+---|
+     | Shift | Z | X | C | V | B | N | M |  | . | / | Shift | Fn0 |        |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
             |------+------+-----------------------+------+------|
             | LAlt | LGUI | ******* Space ******* | RGUI | RAlt |
@@ -29,8 +26,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_DEL, \
   KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
   KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  MO(HHKB),  KC_NO,            \
-  KC_LALT,  KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_SPC,   KC_RGUI,  KC_RALT, KC_RALT,  KC_RALT, KC_RALT),
+  KC_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,     KC_N,    KC_M,     KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,   MO(HHKB),         \
+  KC_LALT,  KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_SPC,   KC_RGUI,  KC_RGUI, KC_RALT,  KC_RALT, KC_RALT),
+
+  
+
+
+/* Game Level: Gaming Layer
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Tab   | Q | W | E | R | T | Y | U | I | O | P | [     | ]   | Backs |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Shift | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+     | Cont  | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
+     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
+
+            |------+------+-----------------------+------+------|
+            | LAlt | LGUI | ******* Space ******* | RGUI | RAlt |
+            |------+------+-----------------------+------+------|
+    */
+
+  [GAME] = KEYMAP(  //  default layer
+  KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_GRV, \
+  KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
+  KC_LSFT,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
+  KC_LCTL,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,     KC_N,    KC_M,     KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,   MO(HHKB),         \
+  KC_LALT,  KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_SPC,   KC_RGUI,  KC_RGUI, KC_RALT,  KC_RALT, KC_RALT),
+
 
 
 
@@ -52,36 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
   [HHKB] = KEYMAP(
-  KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_GRV, \
-  KC_CAPS,  TO(2),    TO(0),    RESET,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_UP,    KC_TRNS,  KC_BSPC,          \
-  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_PAST,  KC_PSLS,  KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RGHT,  KC_PENT,                    \
-  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_TRNS,  KC_TRNS,  KC_PPLS,  KC_PMNS,  KC_END,   KC_PGDN,  KC_DOWN,  KC_TRNS,  KC_TRNS,  KC_TRNS,          \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
-
-
-
-/* BASE Level: Gaming Layer
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | \     | ` |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Tab   | Q | W | E | R | T | Y | U | I | O | P | [     | ]   | Backs |   |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Shift | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Cont  | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
-     |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-
-            |------+------+-----------------------+------+------|
-            | LAlt | LGUI | ******* Space ******* | RGUI | RAlt |
-            |------+------+-----------------------+------+------|
-    */
-
-  [BASE] = KEYMAP(  //  default layer
-  KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,    KC_5,     KC_6,     KC_7,    KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSLS,  KC_GRV, \
-  KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSPC,          \
-  KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,     KC_H,     KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,     KC_N,     KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  MO(HHKB),  KC_NO,            \
-  KC_LALT,  KC_LALT,  KC_LGUI,  KC_SPC,  KC_SPC,  KC_SPC,   KC_RGUI,  KC_RALT, KC_RALT,  KC_RALT, KC_RALT)};
+  KC_PWR,   KC_F1,       KC_F2,       KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_GRV, \
+  KC_CAPS,  TO(BASE),    TO(GAME),    RESET,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_UP,    KC_TRNS,  KC_BSPC,          \
+  KC_VOLD,  KC_VOLU,     KC_MUTE,     KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_PAST,  KC_PSLS,  KC_HOME,  KC_PGUP,  KC_LEFT,  KC_RGHT,  KC_PENT,                    \
+  RGB_TOG,  KC_NO,       RGB_MOD,     RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_END,   KC_PGDN,  KC_DOWN,  KC_TRNS,  KC_TRNS,          \
+  KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)};
 
 
 const uint16_t PROGMEM fn_actions[] = {
